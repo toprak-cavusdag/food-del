6b33b241ef6966ef28d8574d322ac5829e7a236c
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2024-06-26 13:00:07 +0300

    last

--- frontend/src/components/foodItem/FoodItem.jsx
+++ frontend/src/components/foodItem/FoodItem.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContent";
