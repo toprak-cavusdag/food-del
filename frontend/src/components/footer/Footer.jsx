fd623dfb888dea61b91813c53ad30949dbedddb8
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2024-07-04 08:04:07 +0300

    footer done

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -19,9 +19,27 @@ const Footer = () => {
             <img src={assets.linkedin_icon} alt="" />
           </div>
         </div>
-        <div className="footer-content-center"></div>
-        <div className="footer-content-right"></div>
+        <div className="footer-content-center">
+          <h2>Contact</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
       </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 @ tomato.com - All Right Reserved
+      </p>
     </div>
   );
 };
