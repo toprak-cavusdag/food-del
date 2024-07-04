ea6013df0156624feeb5744e28cba339270b8f2a
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2024-07-04 08:13:00 +0300

    App Download done

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import Header from "../../components/header/Header";
 import ExploreMenu from "../../components/exploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/foodDisplay/FoodDisplay";
+import AppDownload from "../../components/appDownload/AppDownload";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -12,6 +13,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
